cd16a39f6e4e681176b8c5526b719c4b36f99c88
benpentecost2004 <123497547+benpentecost2004@users.noreply.github.com>
2023-01-24 14:19:37 -0500

    Add files via upload

---
+++ application.java
@@ -0,0 +1,23 @@
+public class application{
+    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+        int z = 16;
+
+        int a = x + y;
+        int b = x + z;
+        int c = y + z;
+
+        System.out.println("Integer x is equal to " + x);
+        System.out.println("Integer y is equal to " + y);
+        System.out.println("Integer z is equal to " + z);
+
+        System.out.println(" ");
+
+        System.out.println("The sum of integers x and y is equal to " + a);
+        System.out.println("The sum of integers x and z is equal to " + b);
+        System.out.println("The sum of integers y and z is equal to "+ c);
+    }
+
+}
